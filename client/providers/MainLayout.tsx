b7e016ff3bfb5dfc8f869e12b8531174f03ae7d3
Fortune Agbone <130207011+Fortune-O-Agbone@users.noreply.github.com>
2024-11-27 14:25:56 -0700

    Currently building the client side of this project. User can log into the Task Manager  website. TaskFyer Website Layout Setup is now under development

---
+++ client/providers/MainLayout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface MainLayoutProps {
+    children: React.ReactNode;
+}
+
+function MainLayout({ children }: MainLayoutProps){
+    return <div>{children}</div>;
+}
+
+export default MainLayout;
