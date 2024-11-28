0e2b1d777ba76e37faecf439492898221a3f4a42
Fortune Agbone <130207011+Fortune-O-Agbone@users.noreply.github.com>
2024-11-28 16:01:10 -0700

    Chart and Sidebar successfully added

--- client/providers/MainLayout.tsx
+++ client/providers/MainLayout.tsx
@@ -4,8 +4,12 @@ interface MainLayoutProps {
     children: React.ReactNode;
 }
 
-function MainLayout({ children }: MainLayoutProps){
-    return <div>{children}</div>;
+function MainLayout({ children }: MainLayoutProps) {
+    return (
+        <div className="main-layout flex-1 bg-[#EDEDED] border-2 border-white rounded-[1.5rem] overflow-auto">
+            {children}
+        </div>
+    );
 }
 
 export default MainLayout;
