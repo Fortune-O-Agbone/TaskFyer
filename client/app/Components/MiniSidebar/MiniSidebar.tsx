61f0178b2a305bf5e52c2e4f4b2c32f22459f346
Fortune Agbone <130207011+Fortune-O-Agbone@users.noreply.github.com>
2024-11-28 10:53:15 -0700

    Sucessfully completed the inclusion of the MiniSidebar on the Client side of the project

--- client/app/Components/MiniSidebar/MiniSidebar.tsx
+++ client/app/Components/MiniSidebar/MiniSidebar.tsx
@@ -1,9 +1,11 @@
 "use client";
 import IconCheck from "@/public/icons/IconCheck";
+import IconDeleteAll from "@/public/icons/IconDeleteAll";
 import IconFileCheck from "@/public/icons/IconFileCheck";
 import IconGrid from "@/public/icons/IconGrid";
 import IconStopwatch from "@/public/icons/IconStopwatch";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 
@@ -17,7 +19,7 @@ function MiniSidebar() {
 
     const navItems = [
         {
-            icon: <IconGrid />,
+            icon: <IconGrid strokeColor={getStrokeColor("/")}/>,
             title: "All",
             link: "/",
         },
@@ -46,9 +48,22 @@ function MiniSidebar() {
             <div className="mt-8 flex-1 flex flex-col items-center justify-between">
                 <ul className="flex flex-col gap-10">
                     {navItems.map((item, index) => (
-                        <li key={index}></li>
+                        <li key={index} className="relative group">
+                            <Link href={item.link}>{item.icon}</Link>
+
+                            {/* Hover Tooltip */}
+                            <span className="u-triangle absolute top-[50%] translate-y-[-50%] left-8 text-xs pointer-events-none text-white bg-[#3aafae] px-2 py-1 rounded-md shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                {item.title}
+                            </span>
+                        </li>
                     ))}
                 </ul>
+
+                <div className="mb-[1.5rem]">
+                    <button className="w-12 h-12 flex justify-center items-center border-2 border-[#EB4E31]  p-2 rounded-full">
+                        <IconDeleteAll strokeColor="#EB4E31" />
+                    </button>
+                </div>
             </div>
         </div>
     );
