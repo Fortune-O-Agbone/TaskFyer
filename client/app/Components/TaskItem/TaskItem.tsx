a42fe149bde4a62e54723af7931c8e636a67c000
Fortune Agbone <130207011+Fortune-O-Agbone@users.noreply.github.com>
2024-12-06 01:33:02 -0700

    User Profile is now active, webapge animations and motion is now active, and user login and logout features have been implemented as well. Would be making attempts to  focus on deploying the Taskfyer Web Application using a Third Party Service

--- client/app/Components/TaskItem/TaskItem.tsx
+++ client/app/Components/TaskItem/TaskItem.tsx
@@ -3,6 +3,8 @@ import { edit, star, trash } from "@/utils/Icons";
 import { Task } from "@/utils/types";
 import { formatTime } from "@/utils/utilities";
 import React from "react";
+import { motion } from "framer-motion";
+import { item } from "@/utils/animations";
 
 interface TaskItemProps {
     task: Task;
@@ -27,7 +29,10 @@ function TaskItem({ task }: TaskItemProps) {
     const { getTask, openModalForEdit, deleteTask, modalMode } = useTasks();
 
     return (
-        <div className="h-[16rem] px-4 py-3 flex flex-col gap-4 shadow-sm bg-[#f9f9f9] rounded-lg border-2 border-white">
+        <motion.div
+            className="h-[16rem] px-4 py-3 flex flex-col gap-4 shadow-sm bg-[#f9f9f9] rounded-lg border-2 border-white"
+            variants={item}
+        >
             <div>
                 <h4 className="font-bold text-2x1">{task.title}</h4>
                 <p>{task.description}</p>
@@ -58,7 +63,7 @@ function TaskItem({ task }: TaskItemProps) {
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
 
--- client/providers/MainLayout.tsx
+++ client/providers/MainLayout.tsx
@@ -13,6 +13,7 @@ function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="main-layout flex-1 bg-[#EDEDED] border-2 border-white rounded-[1.5rem] overflow-auto">
       {isEditing && <Modal />}
+      {profileModal && <ProfileModal />}
       {children}
     </div>
   );
